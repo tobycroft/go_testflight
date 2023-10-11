d007bf2e9b92a00f02a0fba88236f104c44982cc
火线兔 <silverhawk@foxmail.com>
2023-10-11 09:36:27 +0800

    TuuzMac

---
+++ channnel.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var ChanFlight = make(chan any, 100)
+
+func main() {
+	go readout()
+	writein()
+	time.Sleep(100 * time.Millisecond)
+
+}
+
+func readout() {
+	for c := range ChanFlight {
+		fmt.Println(c)
+		//time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func writein() {
+	for i := 0; i < 100; i++ {
+		ChanFlight <- i
+	}
+}
