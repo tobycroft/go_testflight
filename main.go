bc1aa2b7ded2b651cb31be1ed951ce69ae7f1f64
tuuz <silverhawk@foxmail.com>
2023-10-30 15:14:23 +0800

    TuuzPC

--- main.go
+++ main.go
@@ -8,8 +8,16 @@ import (
 func main() {
 	tuuz.Db()
 	db := tuuz.Db().Table("aaa")
-	db.Where("1=1")
-	//fmt.Println(db.BuildSql("select"))
+	db.Fields()
+	db.Where("id")
+	//db.Where("val")
+	db.Data(map[string]any{
+		"id":   "2",
+		"val":  "ggg",
+		"val2": "sss",
+	})
+	//fmt.Println(db.BuildSql("replace"))
+	fmt.Println(db.Replace())
 	//db.Query()
-	fmt.Println(db.Get())
+	//fmt.Println(db.Get())
 }
